512bbc66302961beee7272ed882a7c013c9be779
EricAlbiol <ealbiol@institutmvm.cat>
2021-12-09 12:39:23 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def main():
+    parells = list()
+    for x in range (0,31):
+        if x % 2 == 0:
+            parells.append(x)
+    print(parells)
+if __name__ == '__main__':
+    main()
+
+
